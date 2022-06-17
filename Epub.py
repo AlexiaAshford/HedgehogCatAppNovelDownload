350ff33c532e41108fe30f2e9c7698377e81d2ec
lifegpc <g1710431395@gmail.com>
2022-06-17 22:43:08 +0800

    EPUB 支持打包图片 支持使用 python-magic 识别正确的mimetype
    EPUB3规范规定的远程资源里不允许包含远程图片（见 https://www.w3.org/publishing/epub3/epub-spec.html#sec-resource-locations ），所以直接移除了对包含远程图片的支持。

--- Epub.py
+++ Epub.py
@@ -1,6 +1,22 @@
 from ebooklib import epub
+from genericpath import exists
+from html.parser import HTMLParser
 from instance import *
+import os
+from posixpath import basename
+try:
+    import magic
+    have_magic = True
+except ImportError:
+    have_magic = False
+    print('Warning: python-magic not found. The mimetype in EPUB file may wrong.')
+    import platform
+    if platform.system() == "Windows":
+        print('python-magic-bin is also needed on Windows.')
+from urllib.parse import urlparse
 import requests
+from typing import List
+import xml.etree.ElementTree as ET
 
 
 def get_cover_image(cover_url: str):
@@ -12,6 +28,115 @@ def get_cover_image(cover_url: str):
         retry += 1
         if retry > 5:
             return None
+
+
+class HTMLImage:
+    def __init__(self, attrs):
+        self.src = None
+        self.alt = None
+        self.path = None
+        self.epub_path = None
+        for key, value in attrs:
+            if key == 'src':
+                self.src = value
+            elif key == 'alt':
+                self.alt = value
+
+    def download_image(self):
+        if not self.is_valid():
+            return False
+        if self.path is None:
+            file_name = basename(urlparse(self.src).path)
+            if file_name == "" and self.alt is not None:
+                file_name = f"{self.alt}.jpg"
+            if file_name == "":
+                return False
+            self.path = os.path.join(Vars.config_text, file_name)
+        if exists(self.path):
+            return True
+        retry = 0
+        while True:
+            response = requests.get(self.src)
+            if response.status_code == 200:
+                break
+            retry += 1
+            if retry > 5:
+                return False
+        with open(self.path, 'wb') as f:
+            f.write(response.content)
+        return True
+
+    def is_valid(self):
+        return self.src is not None
+
+    def to_local(self):
+        if not self.is_valid():
+            return ""
+        if not self.download_image():
+            raise ValueError("Failed to download image.")
+        self.epub_path = os.path.basename(self.path)
+        if have_magic:
+            with open(self.path, 'rb') as f:
+                mime = magic.from_buffer(f.read(4096), True)
+            self.epub_path = os.path.splitext(self.epub_path)[0] + get_extension(mime)
+        d = { 'src': self.epub_path }
+        if self.alt:
+            d['alt'] = self.alt
+        return ET.tostring(ET.Element('img', d), 'unicode')
+
+
+# Used to parse content
+class ContentParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.data = []
+        # Local image file lists
+        self.images = []
+
+    def handle_data(self, data: str):
+        self.data.append(data)
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'img':
+            self.data.append(HTMLImage(attrs))
+        elif tag == 'p':
+            pass
+        else:
+            raise NotImplementedError()
+
+    def have_image(self) -> bool:
+        for i in self.data:
+            if isinstance(i, HTMLImage):
+                if i.is_valid():
+                    return True
+        return False
+
+    def to_local(self) -> str:
+        data = ''
+        for i in self.data:
+            if isinstance(i, str):
+                data += f'<p>{i}</p>\n'
+            elif isinstance(i, HTMLImage):
+                if i.is_valid():
+                    data += i.to_local()
+                    self.images.append(i)
+            else:
+                raise NotImplementedError()
+        return data
+
+
+def get_extension(mime: str) -> str:
+    if mime == 'image/gif':
+        return '.gif'
+    elif mime == 'image/jpeg':
+        return '.jpeg'
+    elif mime == 'image/png':
+        return '.png'
+    elif mime == 'image/svg+xml':
+        return '.svg'
+    else:
+        print(mime)
+        raise NotImplementedError()
 
 
 class EpubFile:
@@ -36,18 +161,34 @@ class EpubFile:
     def download_cover_and_add_epub(self):  # download cover image and add to epub file as cover
         png_file = get_cover_image(Vars.current_book.cover)  # get cover image from url
         if png_file is not None:  # if cover image is not None ,then add to epub file
-            self.epub.set_cover(Vars.current_book.book_name + '.png', png_file)  # add cover image to epub file
+            if have_magic:
+                mime_type = magic.from_buffer(png_file, mime=True)
+                file_name = Vars.current_book.book_name + get_extension(mime_type)
+            else:
+                file_name = Vars.current_book.book_name + '.png'
+            self.epub.set_cover(file_name, png_file)  # add cover image to epub file
 
-    def add_chapter_in_epub_file(self, chapter_title: str, content_lines_list: str, serial_number: str):
+    def add_chapter_in_epub_file(self, chapter_title: str, content_lines_list: List[str], serial_number: str):
         import uuid
         chapter_serial = epub.EpubHtml(
             title=chapter_title,
             file_name=str(serial_number).rjust(4, "0") + '.xhtml',
             lang='zh-CN',
-            uid='u' + uuid.uuid4().hex  # XML name can not start with diget
+            uid='u' + uuid.uuid4().hex,  # XML name can not start with diget
         )
-        chapter_serial.content = '</p>\r\n<p>'.join(content_lines_list)
+        parser = ContentParser()
+        parser.feed('</p>\r\n<p>'.join(content_lines_list))
+        parser.close()
+        chapter_serial.content = parser.to_local()
         self.epub.add_item(chapter_serial)  # add chapter to epub file as item
+        for oimg in parser.images:
+            with open(oimg.path, 'rb') as f:
+                img_content = f.read()
+            img = epub.EpubImage()
+            img.file_name = oimg.epub_path
+            img.content = img_content
+            img.id = 'i' + uuid.uuid4().hex
+            self.epub.add_item(img)
         self.EpubList.append(chapter_serial)  # add chapter to epub list
 
     def save_epub_file(self):  # save epub file to local
